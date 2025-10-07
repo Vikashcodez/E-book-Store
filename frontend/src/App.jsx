fcc975b694db2d6a9a2a7e3b0d439cd1c24e3cae
Vikashcodez <chotuvikash6@gmail.com>
2025-10-07 17:50:59 +0530

    frontend setup done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { Button, HStack } from "@chakra-ui/react"
+
+const App = () => {
+  return (
+    <HStack>
+      <Button>Click me</Button>
+      <Button>Click me</Button>
+    </HStack>
+  )
+}
+export default App
